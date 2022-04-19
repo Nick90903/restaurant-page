0f17f5c8252fc9a50d34bc41426e512426688782
nick90903 <nhatchew03@gmail.com>
2022-04-19 12:11:52 -0500

    Adds header and basic screen change functions

---
+++ src/draw-Main.js
@@ -0,0 +1,10 @@
+const drawMain = () => {
+    console.log('Home Clicked');
+    let temp = document.querySelector('.body-Content');
+    if(temp != null){temp.remove()}
+    
+
+
+}
+
+export {drawMain};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('Working');
+import { drawContact } from './draw-Contact';
+import {drawMain} from './draw-Main';
+import {drawMenu} from './draw-Menu';
+
+
+
+function drawHeader() {
+    const container = document.querySelector('.content');
+    const content = document.createElement('div');
+    content.classList.add('body-Content');
+    console.log(container);
+
+    //creates all header elements
+    const header_Container = document.createElement('div');
+    const home_Button = document.createElement('button');
+    const menu_Button = document.createElement('button');
+    const contact_Button = document.createElement('button');
+
+    header_Container.classList.add('header-Container');
+
+    //Sets up home button and appends
+    home_Button.textContent = ('Home');
+    home_Button.classList.add('home-Buton');
+    home_Button.addEventListener('click', function() {
+        drawMain();
+    })
+    header_Container.appendChild(home_Button);    
+
+    //Set up menu button and appends
+    menu_Button.textContent = ('Menu');
+    menu_Button.classList.add('menu-Button');
+    menu_Button.addEventListener('click', function () {
+        drawMenu();
+    })
+    header_Container.appendChild(menu_Button);
+
+    //Sets up contact us button and appends
+    contact_Button.textContent = ('Contact Us');
+    contact_Button.classList.add('contact-Button');
+    contact_Button.addEventListener('click', function() {
+        drawContact();
+    })
+    header_Container.appendChild(contact_Button);
+    console.log(header_Container);
+
+    container.appendChild(header_Container);
+    container.appendChild(content);
+}
+
+drawHeader();
